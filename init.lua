af5bab3580fe74fea220741c24d505acbb06c77b
Fabian Büdding <fabian.buedding@khs.com>
2026-08-06 08:51:17 +0200

    Added stuff

--- init.lua
+++ init.lua
@@ -466,11 +466,11 @@ require("lazy").setup({
 				-- You can put your default mappings / updates / etc. in here
 				--  All the info you're looking for is in `:help telescope.setup()`
 				--
-				-- defaults = {
-				--   mappings = {
-				--     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-				--   },
-				-- },
+				defaults = {
+					mappings = {
+						i = { ["<C-d>"] = require("telescope.actions").delete_buffer },
+					},
+				},
 				-- pickers = {}
 				extensions = {
 					["ui-select"] = { require("telescope.themes").get_dropdown() },
@@ -491,6 +491,9 @@ require("lazy").setup({
 			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
 			vim.keymap.set("n", "<leader>sG", builtin.git_status, { desc = "[S]earch by [G]it status" })
 			vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+			vim.keymap.set("n", "<leader>se", function()
+				builtin.diagnostics({ severity = vim.diagnostic.severity.ERROR })
+			end, { desc = "[S]earch [E]rrors" })
 			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 			vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 			vim.keymap.set("n", "<leader>sc", builtin.commands, { desc = "[S]earch [C]ommands" })
